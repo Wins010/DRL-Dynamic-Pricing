e570585cde19018dd0d94b11228c14fbf35c6692
Wins Shaju Paulose <winss@amitydubai.ae>
2025-10-21 19:17:24 +0400

    Implemented convex optimization to plan the entire heating schedule for an episode

--- hems/residential_agent.py
+++ hems/residential_agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cvxpy as cp
 
 class ResidentialAgent:
     def __init__(self, setpoint_temp, min_temp, initial_temp=None, max_power=3.5):
@@ -13,18 +14,39 @@ class ResidentialAgent:
         self.indoor_temp = self.initial_temp
         self.last_power = 0.0
 
-    def decide_heating(self, price):
-        # Simple rule-based decision: turn on heating if below min_temp
-        if self.indoor_temp < self.min_temp:
-            power = self.max_power
-        else:
-            power = 0.0
-        self.last_power = power
-        return power
+    def optimize_schedule(self, price_profile, outdoor_temp, fixed_load,
+                          lambda_cost=1.0, lambda_comfort=10.0,
+                          alpha=0.1, heating_effect=0.5):
+        """
+        Full-episode optimization as in the research paper:
+        - price_profile: (T,) array of prices
+        - outdoor_temp: (T,) array of outdoor temperatures
+        - fixed_load: (T,) array of fixed loads (can be ignored/added to agg. demand in env)
+        - lambda_cost: weight for cost term in objective
+        - lambda_comfort: weight for discomfort term
+        - alpha: building thermal loss coefficient
+        - heating_effect: temp increase per unit power
+        Returns: heating_power: (T,) array
+        """
+        T = len(price_profile)
+        u = cp.Variable(T)  # heating power
+        x = cp.Variable(T+1)  # indoor temp trajectory
 
-    def update_temperature(self, outdoor_temp, power_used):
-        # Very basic thermal model: indoor temp moves toward outdoor temp unless heated
-        alpha = 0.1  # how fast temp equalizes
-        heating_effect = 0.5 * power_used  # temp increase per unit power
-        temp_change = alpha * (outdoor_temp - self.indoor_temp) + heating_effect
-        self.indoor_temp += temp_change
+        constraints = [x[0] == self.initial_temp]
+        for t in range(T):
+            # Paper's thermal model: x_{t+1} = x_t + alpha*(outdoor_temp[t] - x_t) + heating_effect*u[t]
+            constraints += [x[t+1] == x[t] + alpha * (outdoor_temp[t] - x[t]) + heating_effect * u[t]]
+            constraints += [u[t] >= 0, u[t] <= self.max_power]
+            constraints += [x[t+1] >= self.min_temp]
+        # Paper's objective: cost + discomfort (only penalize if below setpoint)
+        discomfort = cp.sum(cp.pos(self.setpoint_temp - x[1:]))
+        elec_cost = cp.sum(cp.multiply(price_profile, u))
+        obj = lambda_cost * elec_cost + lambda_comfort * discomfort
+
+        prob = cp.Problem(cp.Minimize(obj), constraints)
+        prob.solve(solver=cp.ECOS, warm_start=True)
+
+        if u.value is None:
+            # If solver fails, fallback to no heating
+            return np.zeros(T)
+        return np.clip(u.value, 0, self.max_power)
